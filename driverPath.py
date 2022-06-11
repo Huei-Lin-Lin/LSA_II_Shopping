1f3c56f1c456ad8edd3aec0e64d6dc2c122721c5
Huei-Lin-Lin <aaaaaa121516202326@gmail.com>
2022-06-12 01:32:47 +0800

    update path

--- driverPath.py
+++ driverPath.py
@@ -1,4 +1,4 @@
 from selenium.webdriver.chrome.service import Service
 
 driverPath = Service(r"/usr/lib/chromium-browser/chromedriver")
-# driverPath = Service(r"C:/chromedriver_win32/chromedriver.exe")
+path = "/usr/lib/chromium-browser/chromedriver"
