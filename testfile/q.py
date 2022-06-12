61f27a6c2d49fa77812980fef313968f9991f7a9
yst200089 <87116920+yst200089@users.noreply.github.com>
2022-06-12 17:27:44 +0800

    v6

--- testfile/q.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
-driver = webdriver.Remote(
-    command_executor='http://selenium:4444/wd/hub',
-    desired_capabilities=DesiredCapabilities.CHROME,
-    )
-
-print(driver)
