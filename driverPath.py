0cbb5511bb879be6a8c223b5bb4df72485c9162b
yst200089 <87116920+yst200089@users.noreply.github.com>
2022-06-12 17:24:54 +0800

    Update driverPath.py

--- driverPath.py
+++ driverPath.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service
 driverPath = Service(r"http://localhost:4444/wd/hub")
 path = "http://localhost:4444/wd/hub"
 
+# docker-compose 整合版路徑
 #driverPath = Service(r"http://selenium:4444/wd/hub")
 #path = "http://selenium:4444/wd/hub"
 
