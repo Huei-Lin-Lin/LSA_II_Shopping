659dc26c9e11889d6b3ff2bef539750dd760fbbe
yst200089 <87116920+yst200089@users.noreply.github.com>
2022-06-12 17:15:20 +0800

    Update driverPath.py

--- driverPath.py
+++ driverPath.py
@@ -4,6 +4,9 @@ from selenium.webdriver.chrome.service import Service
 driverPath = Service(r"http://localhost:4444/wd/hub")
 path = "http://localhost:4444/wd/hub"
 
+#driverPath = Service(r"http://selenium:4444/wd/hub")
+#path = "http://selenium:4444/wd/hub"
+
 # 本機測試用的路徑
 # driverPath = Service(r"C:/chromedriver_win32/chromedriver.exe")
 # path = "C:/chromedriver_win32/chromedriver.exe"  
