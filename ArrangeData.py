e9d4bb552a6e1d5b9aaf0ac9b946a45cafc88858
Huei-Lin-Lin <aaaaaa121516202326@gmail.com>
2022-06-14 00:39:57 +0800

    更新中文數字轉換成 int

--- ArrangeData.py
+++ ArrangeData.py
@@ -1,26 +1,43 @@
 import copy
 import json
+import chinese2Int
 
 # 把數字和單位分開
 def divide_Num_Unit(array):
     sortArray = [] # 整理後的食材量，使用量和單位分開
+    haveNum = False
     for i in range(len(array)):
         num =  ''.join([x for x in array[i] if x.isdigit()])
+        if (num!=''):
+            haveNum = True
         unit = ''.join([i for i in array[i] if not i.isdigit()])
+        # 把 中文數字 轉成數字
+        tempNum, unit = chinese2Int.main(unit) # 都是回傳字串
+        if(haveNum):
+            try:
+                if(int(tempNum)):
+                    num+=int(tempNum)
+            except:
+                print(tempNum)
+        else:
+            num = tempNum
         sortArray.append([num, unit])
+        haveNum = False
     return sortArray
+
 
 # 把數字和單位合在一起
 def combine_Num_Unit(array):
     sortArray = []
     for i in range(len(array)):
-        element = str(array[i][0]) + array[i][1]
+        element = str(array[i][0]) + " " + array[i][1]
         sortArray.append(element)
     return sortArray
 
+# 依照使用者輸入的人數做食譜人數的轉換
+# 例如：原本食譜是 2 人份，但使用者要 6 人份，在這個 function 中會做人數轉換
 def UpdateIngredUnitArr(originalQuantity, crawlHeadcount, userNum):
     sortArray = divide_Num_Unit(originalQuantity) # 整理後的食材量，使用量和單位分開
-    print("ArrangeData : "+sortArray)
     copyArray = copy.deepcopy(sortArray) # 複製一份 sortArray
     # 人數轉換
     for i in range(len(copyArray)):
@@ -37,8 +54,6 @@ def UpdateIngredUnitArr(originalQuantity, crawlHeadcount, userNum):
     updateQuantity = combine_Num_Unit(copyArray)
     return updateQuantity
 
-
-
 def writeJSON(data, jsonPath):
     with open(jsonPath, 'w') as f:
         json.dump(data, f)
@@ -50,6 +65,7 @@ def readJSON(path):
     a = json.loads(f) # 再使用 loads
     return a
 
+# 把字典的 key 和 value 分開變成兩個 List
 def divideDICT(dict):
     keyList = []
     valueList = []
@@ -58,6 +74,7 @@ def divideDICT(dict):
         valueList.append(value)
     return keyList, valueList
 
+# 把剛剛分開的兩個 List ( keyList 和 valueList ) 合成一個字典
 def combineList(list1, list2):
     newDICT = dict(zip(list1,list2))
     return newDICT
@@ -80,5 +97,7 @@ def main():
     newDICT['headcount'] = peopleNum
     newDICT['ingredent'] = afterUnit
     writeJSON(newDICT, "./static/data/afterRecipeData.json")
+
+    print("更新後的數量", newDICT)
 if __name__ == '__main__':
     main()
