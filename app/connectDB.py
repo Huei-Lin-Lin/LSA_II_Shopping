c336d51d0ca19d53e1e5390ca8e2b07591e87d98
Huei-Lin-Lin <aaaaaa121516202326@gmail.com>
2022-06-14 22:58:23 +0800

    更新資料庫設定

--- app/connectDB.py
+++ app/connectDB.py
@@ -1,15 +1,14 @@
-import pymysql
+import mysql.connector
 
 try:
-    mydb = pymysql.connect(
+    mydb = mysql.connector.connect(
         host="localhost",   # 資料庫主機地址
         user="root",        # 資料庫使用者名稱
         passwd="",    # 資料庫密碼
         database="lsa2"   # 直連資料庫，如果資料庫不存在，會輸出錯誤資訊
     )
-    # 生成一個遊標物件(相當於cmd開啟mysql中的 mysql>)
-    cursor = mydb.cursor(cursor=pymysql.cursors.DictCursor)  # 讓資料自動組織成字典
-    
-    # print("連線結果", mydb) # 印出連線結果
+    # 生成一個遊標物件 ( 相當於 cmd 開啟 mysql 中的 mysql> )
+    cursor = mydb.cursor(dictionary = True) # 讓資料自動組織成字典
+    print("連線結果", mydb) # 印出連線結果
 except:
     print("資料庫連接失敗：")
--- app/operationDB.py
+++ app/operationDB.py
@@ -1,21 +1,34 @@
+# import connectDB
+# # from typing import List, Dict
+# # import mysql.connector
+
+# # 連接 DB
+# connectDB.mydb
+
+# # 查詢資料庫菜價
+# def queryFoodPrice(str):
+#     config = {
+#         'user': 'root',
+#         'password': 'root',
+#         'host': 'mysql',
+#         'port': '3306',
+#         'database': 'devopsroles',
+#         'auth_plugin': 'mysql_native_password'
+#     }
+#     cursor = connection.cursor()
+#     cursor.execute('SELECT * FROM test_Table')
+#     results = [{name: color} for (name, color) in cursor]
+#     return result
+
 import connectDB
-# from typing import List, Dict
-# import mysql.connector
 
 # 連接 DB
 connectDB.mydb
 
 # 查詢資料庫菜價
 def queryFoodPrice(str):
-    config = {
-        'user': 'root',
-        'password': 'root',
-        'host': 'mysql',
-        'port': '3306',
-        'database': 'devopsroles',
-        'auth_plugin': 'mysql_native_password'
-    }
-    cursor = connection.cursor()
-    cursor.execute('SELECT * FROM test_Table')
-    results = [{name: color} for (name, color) in cursor]
+    cursor = connectDB.cursor # 讓資料自動組織成字典
+    sql = 'select * from foodprice WHERE foodName LIKE \'%{str}%\' '.format(str=str) # 定義 SQL 語句
+    cursor.execute(sql) # 執行 SQL 語句
+    result = cursor.fetchall() # 獲取返回結果
     return result
