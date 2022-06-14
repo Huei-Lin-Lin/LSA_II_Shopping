77e7c9257f157a3a808a8677af7ae5aeb9421053
Huei-Lin-Lin <aaaaaa121516202326@gmail.com>
2022-06-15 04:10:58 +0800

    database test

--- db/foodprice.sql
+++ db/foodprice.sql
@@ -1,3 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2022-06-14 21:06:30
+-- 伺服器版本： 10.4.22-MariaDB
+-- PHP 版本： 8.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫: `lsa2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `foodprice`
+--
 create database lsa2;
 use lsa2;
 
@@ -7,6 +35,10 @@ CREATE TABLE `foodprice` (
   `price` int(11) NOT NULL,
   `unit` varchar(100) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- 傾印資料表的資料 `foodprice`
+--
 
 INSERT INTO `foodprice` (`id`, `foodName`, `price`, `unit`) VALUES
 (1, '雞蛋', 41, '一斤 / 600 (公克)'),
@@ -54,9 +86,27 @@ INSERT INTO `foodprice` (`id`, `foodName`, `price`, `unit`) VALUES
 (43, '高筋麵粉', 56, '1 kg'),
 (44, '麵粉', 56, '1 kg');
 
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `foodprice`
+--
 ALTER TABLE `foodprice`
   ADD PRIMARY KEY (`id`);
 
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `foodprice`
+--
 ALTER TABLE `foodprice`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
